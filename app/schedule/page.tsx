d775412a8d68962b59b7f60921e1ff3ce271cc21
Preeti-codeChakras <167349543+Preeti-codeChakras@users.noreply.github.com>
2026-09-22 11:19:59 -0700

    Feature/change (#47)
    * add ARCL season
    
    * merge m
    
    * button updates
    
    ---------
    
    Co-authored-by: Preeti <pnorris@costco.com>

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -569,8 +569,8 @@ export default function SchedulePage() {
     border-2
     border-blue-900
     bg-blue-900
-    px-4
-    py-2.5
+    px-2.8
+    py-2
     text-sm
     font-semibold
     text-white
